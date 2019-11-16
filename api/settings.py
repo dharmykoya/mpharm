0010a3e94a7ff5361f6f0de00a1a03d72d1093d9
Damilola Adekoya <dharmykoya38@gmail.com>
2019-11-16 15:40:16 +0100

    chore(diagnosis-table):to create a table for diagnosis To install pyscopg2 To setup postgreSQL

--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'diagnosis'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,20 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mpharm',
+        'USER': 'postgres',
+        'HOST': 'localhost', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
     }
 }
 
--- diagnosis/models.py
+++ diagnosis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Diagnosis(models.Model):
+    category_code = models.CharField(max_length=500)
+    diagnosis_code = models.CharField(max_length=500)
+    full_code = models.CharField(max_length=500)
+    abbreviated_description = models.CharField(max_length=500)
+    full_description = models.CharField(max_length=500)
+    category_title = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.category_title
